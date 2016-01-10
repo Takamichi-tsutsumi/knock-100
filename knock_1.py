cdd38d87de0092490b15f6c0b4009d90af52f6e0
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-01-11 03:18:58 +0900

    fix code

--- knock_1.py
+++ knock_1.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 
 # 00.文字列の逆転
-def switch(str):
-    length = len(str)
-    new_str = ""
-    for i in range(length):
-        new_str += str[length - 1 - i]
-    return new_str
-
-print  switch("stressed")
+# def switch(str):
+#     length = len(str)
+#     new_str = ""
+#     for i in range(length):
+#         new_str += str[length - 1 - i]
+#     return new_str
+#
+# print  switch("stressed")
+#
+str = "stressed"
+print str[::-1]
 
 
 # 01.パタトクカシー
 str = u"パタトクカシー"
-new_str = ""
-for i in range(len(str)/2 + 1):
-    new_str += str[2 * i]
-print new_str
-
+print str[::2]
+# new_str = ""
+# for i in range(len(str)/2 + 1):
+#     new_str += str[2 * i]
+# print new_str
 
 
 # 02 パトカー + タクシー
 pat = u"パトカー"
 taxi = u"タクシー"
-str = ""
-for i in range(len(pat)):
-    str += pat[i]
-    str += taxi[i]
-print str
+print(''.join([p + t for (p,t) in zip(pat, taxi)]))
