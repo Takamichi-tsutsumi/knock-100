d6f95131bfe9a7d5dcabdb628e7f6ab041319267
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-01-11 03:07:47 +0900

    ~ 1-02

--- knock_1.py
+++ knock_1.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 # 00.文字列の逆転
 def switch(str):
@@ -11,3 +11,20 @@ def switch(str):
 print  switch("stressed")
 
 
+# 01.パタトクカシー
+str = u"パタトクカシー"
+new_str = ""
+for i in range(len(str)/2 + 1):
+    new_str += str[2 * i]
+print new_str
+
+
+
+# 02 パトカー + タクシー
+pat = u"パトカー"
+taxi = u"タクシー"
+str = ""
+for i in range(len(pat)):
+    str += pat[i]
+    str += taxi[i]
+print str
