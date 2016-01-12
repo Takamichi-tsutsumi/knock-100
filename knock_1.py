8514826ffc18e48e71f4ce17fe0081fb3b8e4e37
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-01-12 23:16:19 +0900

    03, 04

--- knock_1.py
+++ knock_1.py
@@ -27,3 +27,29 @@ print str[::2]
 pat = u"パトカー"
 taxi = u"タクシー"
 print(''.join([p + t for (p,t) in zip(pat, taxi)]))
+
+
+# 03 円周率
+str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+num = []
+
+for word in str.split(" "):
+    word = word.replace(",", "")
+    word = word.replace(".", "")
+    num.append(len(word))
+
+print num
+
+
+# 04 元素記号
+str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+elements = {}
+for i, word in enumerate(str.replace(",","").replace(".","").split(" ")):
+    if i in [1,5,67,8,9,15,16]:
+        elements[word[0:2]] = i + 1
+    else:
+        elements[word[0]] = i + 1
+
+print elements
+
+
