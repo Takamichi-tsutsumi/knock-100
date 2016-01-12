0ba2dcab852145e8c2286b9cc94c4451ac496659
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-01-13 00:59:11 +0900

    07, 08

--- knock_1.py
+++ knock_1.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import string
 
 # 00.文字列の逆転
 # def switch(str):
@@ -10,13 +11,13 @@
 #
 # print  switch("stressed")
 #
-str = "stressed"
-print str[::-1]
+string = "stressed"
+print string[::-1]
 
 
 # 01.パタトクカシー
-str = u"パタトクカシー"
-print str[::2]
+string = u"パタトクカシー"
+print string[::2]
 # new_str = ""
 # for i in range(len(str)/2 + 1):
 #     new_str += str[2 * i]
@@ -30,10 +31,10 @@ print(''.join([p + t for (p,t) in zip(pat, taxi)]))
 
 
 # 03 円周率
-str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+string = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 num = []
 
-for word in str.split(" "):
+for word in string.split(" "):
     word = word.replace(",", "")
     word = word.replace(".", "")
     num.append(len(word))
@@ -42,9 +43,9 @@ print num
 
 
 # 04 元素記号
-str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+string = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 elements = {}
-for i, word in enumerate(str.replace(",","").replace(".","").split(" ")):
+for i, word in enumerate(string.replace(",","").replace(".","").split(" ")):
     if i in [1,5,67,8,9,15,16]:
         elements[word[0:2]] = i + 1
     else:
@@ -52,4 +53,23 @@ for i, word in enumerate(str.replace(",","").replace(".","").split(" ")):
 
 print elements
 
+
+# 07 テンプレートによる文字生成
+
+def template(x,y,z):
+    string = str(x) + "時の" + str(y) + "は" + str(z)
+    return string
+
+print template(12, "気温", 22.4)
+
+
+# 08 暗号文
+
+def cipher(string):
+    new_string = ""
+    for char in string:
+        if char.isalpha() && char.islower():
+            new_string += chr(219 - ord(char))
+        else:
+            new_string += char
 
