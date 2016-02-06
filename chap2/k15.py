3a10eda3875b3fce18d8de68c19a322ae672b03f
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-02-06 20:23:42 +0900

    Modify k15

--- chap2/k15.py
+++ chap2/k15.py
@@ -6,6 +6,6 @@ src, N = open(sys.argv[1], 'r'), int(sys.argv[2])
 src_lines = src.readlines()
 
 for i in range(0, N):
-    print(src_lines[len(src_lines) - (N - i)])
+    print(src_lines[len(src_lines) - (N - i)].replace('\n', ''))
 
 src.close()
